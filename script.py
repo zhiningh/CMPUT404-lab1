edce62450b8d87309413970830bfaeb9a1f63edd
zhininghjl <zhininghjl@gmail.com>
2021-09-05 22:27:13 -0600

    curl step 10 modified script to get source code

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
 import requests
 
+# Get the version of the requests library
 print(requests.__version__)
 
-
+# Get the Google homepage
 print(requests.get("http://google.com/"))
+
+# Get scripts source code from GitHub
+print(requests.get("https://raw.githubusercontent.com/zhininghjl/404_lab_1/master/script.py"
+).content)
