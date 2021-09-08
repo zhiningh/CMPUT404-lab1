b1b6cf91778069b9fcb538787d05f1c0207c950b
zhininghjl <zhininghjl@gmail.com>
2021-09-08 00:53:24 -0600

    add download

--- script.py
+++ script.py
@@ -6,6 +6,8 @@ print(requests.__version__)
 # Get the Google homepage
 print(requests.get("http://google.com/"))
 
-# Get scripts source code from GitHub
-print(requests.get("https://raw.githubusercontent.com/zhininghjl/404_lab_1/master/script.py"
-).content)
+# Get scripts source code from GitHub and download
+code = requests.get("https://raw.githubusercontent.com/zhininghjl/404_lab_1/master/script.py"
+).content
+print(code)
+open('lab_1_downloaded.py', 'wb').write(code)
